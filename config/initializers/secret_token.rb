04c0a69313a17b420c33d27ade969f08ac07a253
Nelson Blaha <nelson.blaha@gmail.com>
2013-07-25 17:14:14 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IpcamForGlass::Application.config.secret_token = 'c5bc89991fc152e036b5baa9c6a3496966856ab019a82b34d4bf319c98097e554156bc24f94631df95d6778964b8dfb5e13ab680e11f52f26141c36b72420285'
