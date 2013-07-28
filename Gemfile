37a007c1fb98939dd6490f64387ae385e13450ec
Nelson Blaha <nelson.blaha@gmail.com>
2013-07-28 18:36:40 -0500

    Google login and Mirror gem

--- Gemfile
+++ Gemfile
@@ -2,11 +2,14 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 gem 'rails', '4.0'
 gem 'sqlite3'
+
 group :assets do
   gem 'sass-rails'
   gem 'coffee-rails'
   gem 'uglifier', '>= 1.0.3'
 end
+
+gem 'mirror-api'
 gem 'jquery-rails'
 gem 'figaro'
 gem 'omniauth'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 IpcamForGlass::Application.routes.draw do
   root :to => 'cameras#index'
+  
+  # session
   get 'signin', to: 'sessions#new', as: :signin
   get 'signout', to: 'sessions#destroy', as: :signout
+  get '/auth/:provider/callback', to: 'sessions#create'
+  
   resources :cameras
 end
